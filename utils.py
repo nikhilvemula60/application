2d83079d7f3d780658603a189012c0c8c518c568
vemulaharshith <30391782+vemulaharshith@users.noreply.github.com>
2022-09-06 21:35:57 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,8 +8,6 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout
 from tensorflow.keras.models import Sequential
-from tensorflow.keras import regularizers
-from tensorflow.keras.optimizers import Adam
 
 
 
