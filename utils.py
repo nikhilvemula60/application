ea5e163de4c4ca778cd0682c4d545664aa9f77fd
vemulaharshith <30391782+vemulaharshith@users.noreply.github.com>
2022-09-07 01:08:07 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -43,16 +43,15 @@ def _windowize_data(data, n_prev):
     return x, y
 
 def create_model(num_words, n_prev):
+    optimizer = keras.optimizers.Adam(learning_rate=.0001)
     model = Sequential()
     model.add(Embedding(num_words, 128, input_length=n_prev))
-    model.add(Bidirectional(LSTM(150, return_sequences=True)))
+    model.add(Bidirectional(LSTM(150,input_shape=(n_prev,1), return_sequences=True)))
     model.add(Dropout(0.3))
     model.add(Bidirectional(LSTM(96)))
-    model.add(Dense(num_words/2, activation='relu', kernel_regularizer=regularizers.l2(0.01)))
     model.add(Dense(num_words, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics='accuracy')
     return model
-
-
 
 def generate_poetry_words(seed_text, poetry_length, n_lines, model):
   for i in range(n_lines):
