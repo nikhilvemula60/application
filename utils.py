4142faa7e075a4133f9622462e31e15e29cb7181
vemulaharshith <30391782+vemulaharshith@users.noreply.github.com>
2022-09-06 22:13:30 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow.keras as keras
 from sklearn.model_selection import train_test_split
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional, Dropout
 from tensorflow.keras.models import Sequential
-
-
+from tensorflow.keras.layers import Dropout, LSTM, Dense, Embedding , Bidirectional
 
 n_prev = 100
 tokenizer = Tokenizer()
